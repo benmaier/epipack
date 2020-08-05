e53ff5f35523d3c7fbbc4da6a2a51a9d15fddff6
Benjamin Maier <benjaminfrankmaier@gmail.com>
2020-08-05 18:11:42 +0200

    added possibility to numerically integrate symbolic models

--- epipack/symbolic_epi_models.py
+++ epipack/symbolic_epi_models.py
@@ -79,6 +79,7 @@ class SymbolicEpiModel(DeterministicEpiModel):
         self.linear_rates = sympy.zeros(self.N_comp, self.N_comp)
         self.quadratic_rates = [ sympy.zeros(self.N_comp, self.N_comp)\
                                  for c in range(self.N_comp) ]
+        self.parameter_values = {}
 
     def set_linear_rates(self,rate_list,reset_rates=True,allow_nonzero_column_sums=True):
         """
@@ -380,7 +381,87 @@ class SymbolicEpiModel(DeterministicEpiModel):
             fixed_point[compartment] = 0
         fixed_point.update(fixed_point_dict)
         return fixed_point.items()
+
+    def set_parameter_values(self,parameter_values):
+        """
+        Set numerical values for the parameters of this model
+
+        Parameters
+        ==========
+        parameter_values : dict
+            A dictionary mapping compartment symbols to
+            numerical values.
+        """
+        self.parameter_values = parameter_values
         
+    def integrate_and_return_by_index(self,
+                                      time_points,
+                                      return_compartments=None,
+                                      integrator='dopri5',
+                                      adopt_final_state=False,
+                                      ):
+        r"""
+        Returns values of the given compartments at the demanded
+        time points (as a numpy.ndarray of shape 
+        ``(return_compartments), len(time_points)``.
+
+        Parameters
+        ==========
+        time_points : np.ndarray
+            An array of time points at which the compartment values
+            should be evaluated and returned.
+        return_compartments : list, default = None
+            A list of compartments for which the result should be returned.
+            If ``return_compartments`` is None, all compartments will
+            be returned.
+        integrator : str, default = 'dopri5'
+            Which method to use for integration. Currently supported are
+            ``'euler'`` and ``'dopri5'``. If ``'euler'`` is chosen,
+            :math:`\delta t` will be determined by the difference
+            of consecutive entries in ``time_points``.
+        adopt_final_state : bool, default = False
+            Whether or not to adopt the final state of the integration
+        """
+
+        these_symbols = [sympy.symbols("t")] + self.compartments
+
+        params = list(self.parameter_values.items())
+        param_symbols = set([_p[0] for _p in params])
+
+        odes = [ ode.subs(params) for ode in self.dydt() ]
+
+        print(odes)
+
+        not_set = []
+        for ode in odes:
+            not_set.extend(ode.free_symbols)
+
+        not_set = set(not_set) - set(these_symbols)
+
+        if len(not_set) > 0:
+            raise ValueError("Parameters", set(not_set), "have not been set. Please set them using",
+                             "SymbolicEpiModel.parameter_values()")
+
+        F_sympy = sympy.lambdify(these_symbols, odes)
+
+        def dydt(t, y, *args, **kwargs):
+            these_args = [t] + y.tolist()
+            return np.array(F_sympy(*these_args))
+
+        if integrator == 'dopri5':
+            result = integrate_dopri5(dydt, time_points, self.y0)
+        else:
+            result = integrate_euler(dydt, time_points, self.y0)
+
+        if adopt_final_state:
+            self.y0 = result[:,-1]
+
+        if return_compartments is not None:
+            ndx = [self.get_compartment_id(C) for C in return_compartments]
+            result = result[ndx,:]
+
+        return result
+
         #def set_initial_conditions(self, initial_conditions):
     #    """
     #    """
@@ -577,4 +658,56 @@ if __name__=="__main__":    # pragma: no cover
             (I, x, eta/(1-I), I, I),
             (I, rho, x),
         ])
-    print(SIS.get_eigenvalues_at_disease_free_state())
+    try:
+        print(SIS.get_eigenvalues_at_disease_free_state())
+    except ValueError as e:
+        print(e)
+
+    print("===========")
+    S, I = sympy.symbols("S I")
+    epi = SymbolicSISModel(eta, rho)
+
+    epi.set_initial_conditions({S: 1-0.01, I:0.01 })
+    epi.set_parameter_values({eta:2,rho:1})
+
+    tt = np.linspace(0,10,1000)
+    result = epi.integrate(tt)
+
+    import matplotlib.pyplot as pl
+
+    pl.plot(tt, result[S])
+    pl.plot(tt, result[I])
+
+    print("===========")
+    t, S, I = sympy.symbols("t S I")
+    epi = SymbolicSISModel((1.5+sympy.cos(t))*eta, rho)
+
+    epi.set_initial_conditions({S: 1-0.2, I:0.2 })
+    epi.set_parameter_values({eta:2,rho:1})
+
+    tt = np.linspace(0,20,1000)
+    result = epi.integrate(tt)
+
+    import matplotlib.pyplot as pl
+
+    pl.figure()
+    pl.plot(tt, result[S])
+    pl.plot(tt, result[I])
+
+    print("===========")
+    t, S, I = sympy.symbols("t S I")
+    epi = SymbolicSISModel(eta/(1-S), rho)
+
+    epi.set_initial_conditions({S: 1-0.2, I:0.2 })
+    epi.set_parameter_values({eta:2,rho:1})
+
+    tt = np.linspace(0,5,1000)
+    result = epi.integrate(tt)
+
+    import matplotlib.pyplot as pl
+
+    pl.figure()
+    pl.plot(tt, result[S])
+    pl.plot(tt, result[I])
+    pl.show()
+
